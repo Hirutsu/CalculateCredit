638b42983ad25959cc87a963c1b4fadbcdfcfe58
Levan Burchuladze <levan.burchuladze@rokolabs.com>
2022-06-14 01:23:15 +0400

    release calucaler credit

---
+++ CalculateCredit/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace CalculateCredit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.summaTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.termTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.procentTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.monthlyTB = new System.Windows.Forms.TextBox();
+            this.procentITB = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.procentValueTB = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.resultSumTB = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сумма кредита";
+            // 
+            // summaTB
+            // 
+            this.summaTB.Location = new System.Drawing.Point(103, 6);
+            this.summaTB.Name = "summaTB";
+            this.summaTB.Size = new System.Drawing.Size(142, 20);
+            this.summaTB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "в рублях";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "в месяцах";
+            // 
+            // termTB
+            // 
+            this.termTB.Location = new System.Drawing.Point(103, 32);
+            this.termTB.Name = "termTB";
+            this.termTB.Size = new System.Drawing.Size(142, 20);
+            this.termTB.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Срок";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "в годовых";
+            // 
+            // procentTB
+            // 
+            this.procentTB.Location = new System.Drawing.Point(103, 58);
+            this.procentTB.Name = "procentTB";
+            this.procentTB.Size = new System.Drawing.Size(142, 20);
+            this.procentTB.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Процент";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Подсчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultSumTB);
+            this.groupBox1.Controls.Add(this.procentValueTB);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.procentITB);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.monthlyTB);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(15, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 177);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Размер ежемесячного платежа";
+            // 
+            // monthlyTB
+            // 
+            this.monthlyTB.Location = new System.Drawing.Point(25, 32);
+            this.monthlyTB.Name = "monthlyTB";
+            this.monthlyTB.Size = new System.Drawing.Size(166, 20);
+            this.monthlyTB.TabIndex = 1;
+            // 
+            // procentITB
+            // 
+            this.procentITB.Location = new System.Drawing.Point(210, 32);
+            this.procentITB.Name = "procentITB";
+            this.procentITB.Size = new System.Drawing.Size(166, 20);
+            this.procentITB.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 16);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Доля % в месяц";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(69, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Сумма с %";
+            // 
+            // procentValueTB
+            // 
+            this.procentValueTB.Location = new System.Drawing.Point(210, 80);
+            this.procentValueTB.Name = "procentValueTB";
+            this.procentValueTB.Size = new System.Drawing.Size(166, 20);
+            this.procentValueTB.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(257, 64);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Переплата";
+            // 
+            // resultSumTB
+            // 
+            this.resultSumTB.Location = new System.Drawing.Point(25, 80);
+            this.resultSumTB.Name = "resultSumTB";
+            this.resultSumTB.Size = new System.Drawing.Size(166, 20);
+            this.resultSumTB.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 214);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.procentTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.termTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.summaTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculate Credit";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox summaTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox termTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox procentTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox monthlyTB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox procentITB;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox procentValueTB;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox resultSumTB;
+    }
+}
+
---
+++ CalculateCredit/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateCredit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double S = Convert.ToDouble(summaTB.Text);
+            double P = Convert.ToDouble(procentTB.Text) / (100 * 12);
+            double N = Convert.ToDouble(termTB.Text);
+            double x = S * (P + (P / (Math.Pow((1 + P), N) - 1)));
+            double I = S * P;
+            double sumWithProcent = x * N;
+            double resultSum = x * N;
+            double procentValue = resultSum - S;
+
+            monthlyTB.Text = x.ToString();
+            procentITB.Text = I.ToString();
+            resultSumTB.Text = resultSum.ToString();
+            procentValueTB.Text = procentValue.ToString();
+        }
+    }
+}
